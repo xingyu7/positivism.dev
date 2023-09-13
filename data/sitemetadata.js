45e5bcaf7ab900b31275cb246c27d7c038407779
Xingyu Liu <xingyuliu@outlook.sg>
2023-09-14 04:03:19 +0800

    Initial Commit

---
+++ data/sitemetadata.js
@@ -0,0 +1,24 @@
+const siteMetadata = {
+  title: "Positivism 实证主义",
+  author: "刘星宇",
+  authorDesc:"从理性的无知到理性的非理性",
+  publishName: "Positivism",
+  headerTitle: "不曾远去的求学生涯",
+  description:
+    "在知识幻觉和实证中摇摆",
+  language: "zh",
+  keywords:['positivism', '实证','刘星宇','Xingyu'],
+  siteUrl: "https://positivism.dev",
+  siteRepo: "positivism.dev",
+  repoid:"R_kgDOKJn3Lw",
+  categoryid: "DIC_kwDOKJn3L84CYv5h",
+  siteLogo: "/static/favicons/favicon.ico",
+  image: "/static/favicons/avatar.png",
+  cover: "/static/images/cover.jpg",
+  email: "xingyuliu@outlook.sg",
+  github: "xingyu7",
+  twitter: "@positivismdev",
+  twitterid:"1667878618687737856",
+};
+
+module.exports = siteMetadata;
